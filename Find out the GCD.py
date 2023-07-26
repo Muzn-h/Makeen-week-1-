e12fdc7045a97f8395950eed9c67d4d74f8ae833
muzn-h <18-0188@student.gutech.edu.om>
2023-07-26 16:45:52 +0400

    problem solving

---
+++ Find out the GCD.py
@@ -0,0 +1,12 @@
+#ask the user to input two number and find the GCD
+import math
+
+a=int(input("enter the first number:"))
+b=int(input("enter the second number:"))
+print("The GCD OF ",a , b )
+print(math.gcd(a,b))
+
+
+
+
+
---
+++ problem A.py
@@ -0,0 +1,6 @@
+#conunt the distenation between ali and his job 
+x=int(input("enter the speed"))
+y=int(input("enter the time:"))
+
+d=x*y
+print(x,"*",y,"=",d)
